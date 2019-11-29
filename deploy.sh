d3ca3532eb692968e68765faa152b42b1d5a0bf4
chenli <yli_chen@kuaidi100.com>
2019-11-29 09:07:44 +0800

    一点改变

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io
-git push -f git@github.com:chenli-wuyi/li_chen.github.io.git master
+git push -f git@github.com:chenli-wuyi/chenli-wuyi.github.io.git master
 
 cd -
